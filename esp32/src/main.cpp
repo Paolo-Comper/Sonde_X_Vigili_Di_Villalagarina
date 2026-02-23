686d3499fe08b0edbbde181742f2efc3bad7c707
Paolo-Comper <rpaolo609@gmail.com>
2026-02-23 12:15:47 +0100

    codice finito da testare

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,7 +1,6 @@
 #include "Arduino.h"
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <ArduinoJson.h>
 
 static const char* wifi_ssid  = "Paolo";
 static const char* wifi_pass  = "12345678";
@@ -9,12 +8,12 @@ static const char* wifi_pass  = "12345678";
 static const char* mqtt_server = "test.mosquitto.org";
 static const int   mqtt_port   = 1883;
 
-static const char* mqtt_topic  = "sensors/data";
+static const char* mqtt_topic  = "Sonde_X_Vigili_Di_Villalagarina/fiume1";
 
 WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
 
-float water_level = 2.9;
+float water_level = 2.9f;
 
 void wifi_connect()
 {
@@ -26,18 +25,18 @@ void wifi_connect()
         delay(500);
         Serial.print(".");
     }
+
     Serial.println("\nWiFi connected");
 }
 
 void mqtt_reconnect()
 {
-    // setServer prima di tentare di connettere
     mqtt_client.setServer(mqtt_server, mqtt_port);
 
     while (!mqtt_client.connected())
     {
         Serial.print("Attempting MQTT connection...");
-        // usa la firma base con solo clientID
+
         if (mqtt_client.connect("esp32_fiume1"))
         {
             Serial.println("connected");
@@ -53,18 +52,23 @@ void mqtt_reconnect()
 
 void send_sensor()
 {
-    StaticJsonDocument<256> doc;
-
-    doc["id"]  = "fiume1";
-    doc["value"] = water_level;
-    doc["lat"] = 45.890;
-    doc["lon"] = 11.040;
-
     char buffer[256];
-    size_t n = serializeJson(doc, buffer);
 
-    // pubblica con lunghezza precisa
-    mqtt_client.publish(mqtt_topic, buffer, n);
+    int written = snprintf(
+        buffer,
+        sizeof(buffer),
+        R"({ "id": "fiume1", "value": %.4f, "lat": 45.890, "lon": 11.040 })",
+        water_level
+    );
+
+    if (written > 0 && written < (int)sizeof(buffer))
+    {
+    mqtt_client.publish(mqtt_topic, buffer, written);
+    }
+    else
+    {
+        Serial.println("Errore costruzione JSON");
+    }
 }
 
 void setup()
@@ -72,8 +76,6 @@ void setup()
     Serial.begin(115200);
 
     wifi_connect();
-
-    // imposta server MQTT qui oppure nel reconnect
     mqtt_client.setServer(mqtt_server, mqtt_port);
 }
 
